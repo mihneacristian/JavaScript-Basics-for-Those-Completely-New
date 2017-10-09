111cc10994aba282b54800c7352099ae40207d3c
Popa Mihnea Cristian <mihnea-cristian.popa@outlook.de>
2017-10-09 21:54:29 +0300

    Add files via upload

---
+++ Get_Data_from_User.js
@@ -0,0 +1,5 @@
+var VisitorAge = prompt("How old are you ?");
+var VisitorName = prompt("What is your name ?");
+document.write("Your are " + VisitorAge + " years old and your name is " + VisitorName + ".");
+
+Console.log("This message will be sent to the Console. The message will let us know that our code is working correctly");
