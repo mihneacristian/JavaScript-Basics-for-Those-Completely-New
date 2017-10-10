dd5616148d3cc2a7a093205f5a4d0c35839f451e
Popa Mihnea Cristian <mihnea-cristian.popa@outlook.de>
2017-10-10 21:42:41 +0300

    Add files via upload

---
+++ Variables_and_Strings_Challenge.js
@@ -0,0 +1,6 @@
+alert("Welcome to my webpage");
+var UserName = prompt("What is your name ?");
+var UserFavColor = prompt("What is your favorite color ?");
+document.write("Hello " + UserName + " ,your favorite color is " + UserFavColor + ".");
+
+console.log("Program Complete");
