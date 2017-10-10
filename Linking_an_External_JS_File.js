8a2f63a142dc648c6770d2dc46a347634bc3de0a
Popa Mihnea Cristian <mihnea-cristian.popa@outlook.de>
2017-10-10 21:43:02 +0300

    Update Linking_an_External_JS_File.js

--- Linking_an_External_JS_File.js
+++ Linking_an_External_JS_File.js
@@ -1,2 +1,2 @@
 document.write("Hello from the external JavaScript file !")
-Console.log("This message will be sent to the Console. The message will let us know that our code is working correctly");
+console.log("This message will be sent to the Console. The message will let us know that our code is working correctly");
