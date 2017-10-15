f05e447e1ab2f71ebf9624fa74b29638f25a5648
Popa Mihnea Cristian <mihnea-cristian.popa@outlook.de>
2017-10-15 21:00:57 +0300

    Delete Conditional_Statements.js

--- Conditional_Statements.js
+++
@@ -1,8 +0,0 @@
-var UserQuestion = prompt("What is 2+2 ?");
-    if (UserQuestion === "4") {
-        alert("Good job ! " + UserQuestion + " is the correct answer !" );
-        document.write("You are a math genius !");    
-} else {
-        alert("Sorry, that is not the right answer :(");
-        document.write("Please try again !");
-};
