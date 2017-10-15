998cf0065eeea33807c0f3482c54d21293fa21c6
Popa Mihnea Cristian <mihnea-cristian.popa@outlook.de>
2017-10-15 19:14:06 +0300

    Add files via upload

---
+++ Create_Random_Numbers.js
@@ -0,0 +1,4 @@
+var RandomNumber = Math.random(); // or multiply our variable with a number, ex. var RandomNumber = Math.random () * 15;
+var RandomNumber1 = Math.floor(Math.random() * 16) +1; //Math.floor method
+// alert (RandomNumber);
+alert (RandomNumber1);
