9b13963f5cfc36f17ef23b1bdb8480105f50a581
Popa Mihnea Cristian <mihnea-cristian.popa@outlook.de>
2017-10-15 20:19:09 +0300

    Add files via upload

---
+++ Random_Number_Challenge.js
@@ -0,0 +1,5 @@
+var UserNumber = prompt("Pick a random number");
+UserNumber = parseInt(UserNumber);
+UserNumber = Math.floor(Math.random() * UserNumber) +1;
+alert ("Your random generated number is " + UserNumber);
+console.log("The program ran successfully");
